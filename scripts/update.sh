f4b22af02ee7d57bc7325722f955aed53b8a2ea6
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-08-01 17:41:53 +0200

    script: Updates

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,39 +1,42 @@
 #!/bin/bash
 
 set +e
+sudo apt-get update > /dev/null
+sudo apt-get install -y jq > /dev/null
 
-send_message() {
-	curl -s -X POST https://api.telegram.org/bot$TG_BOT_TOKEN/sendMessage -d chat_id="$1" -d text="$(printf "$2")"
+edit_ci_message() {
+	MESSAGE_TEXT="$MESSAGE_TEXT
+$1"
+	curl -s -X POST https://api.telegram.org/bot$TG_BOT_TOKEN/editMessageText \
+		-d chat_id="$TG_FLUID_MAINTAINERS_CHAT_ID" -d message_id="$MESSAGE_ID" -d text="FluidCI
+New commit has been pushed ($LAST_COMMIT)
+$MESSAGE_TEXT"
 }
 
-syntax_check() {
+LAST_COMMIT=$(git rev-parse --short HEAD)
+
+MESSAGE_ID=$(curl -s -X POST https://api.telegram.org/bot$TG_BOT_TOKEN/sendMessage -d chat_id="$TG_FLUID_MAINTAINERS_CHAT_ID" -d text="FluidCI started" | jq .result.message_id)
+
+edit_ci_message "First step: Checking changed JSONs"
+
+CHANGED_JSONS="$(git diff --name-only HEAD~1 . | grep *.json)"
+if [ "$CHANGED_JSONS" = "" ]; then
+	edit_ci_message "No JSON has been added or changed!"
+	exit
+else
 	for json in $CHANGED_JSONS; do
 		jq . $json
 		if [ "$?" = 0 ]; then
-			send_message "$TG_FLUID_MAINTAINERS_CHAT_ID" "$json has passed the syntax test"
+			edit_ci_message "$json has passed the syntax test"
 		else
-			send_message "$TG_FLUID_MAINTAINERS_CHAT_ID" "$json has failed the syntax test"; SYNTAX_CHECK_RESULT="failed"
+			edit_ci_message "$json has failed the syntax test"
+			SYNTAX_CHECK_RESULT="failed"
 		fi
 	done
-	if [ "$SYNTAX_CHECK_RESULT" = "" ]; then
-		SYNTAX_CHECK_RESULT="success"
-	fi
-}
-
-sudo apt-get update
-sudo apt-get install -y jq
-
-send_message "$TG_FLUID_MAINTAINERS_CHAT_ID" "Fluid CI\nFound a new commit\n\nFirst step: Checking changed JSONs"
-
-CHANGED_JSONS="$(git diff --name-only HEAD~1 . | grep *.json)"
-
-if [ "$CHANGED_JSONS" = "" ]; then
-	send_message "$TG_FLUID_MAINTAINERS_CHAT_ID" "No JSON has been added or changed!"
-else
-	syntax_check
 	if [ "$SYNTAX_CHECK_RESULT" != "failed" ]; then
-		send_message "$TG_FLUID_MAINTAINERS_CHAT_ID" "All modified JSONs are correct!"
+		edit_ci_message "All modified JSONs are correct!"
 	else
-		send_message "$TG_FLUID_MAINTAINERS_CHAT_ID" "There's a bad JSONs!\nIt needs to be fixed ASAP\n@SebaUbuntu kill whoever did this commit and fix it"
+		edit_ci_message "$TG_FLUID_MAINTAINERS_CHAT_ID" "There's a bad JSONs! @SebaUbuntu kill whoever did this commit and fix it"
+		exit
 	fi
 fi
