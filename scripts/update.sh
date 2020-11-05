510e0abb327f97052b60beb3a04c73bb5ad98b93
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-11-05 11:06:40 +0100

    scripts: Fix syntax checker

--- scripts/update.sh
+++ scripts/update.sh
@@ -7,19 +7,27 @@ sudo apt-get install -y jq > /dev/null
 edit_ci_message() {
 	MESSAGE_TEXT="$MESSAGE_TEXT
 $1"
-	curl -s -X POST https://api.telegram.org/bot$TG_BOT_TOKEN/editMessageText \
-		-d chat_id="$TG_FLUID_MAINTAINERS_CHAT_ID" -d message_id="$MESSAGE_ID" -d text="FluidCI
-New commit has been pushed ($LAST_COMMIT)
-$MESSAGE_TEXT"
+	curl -s -X POST https://api.telegram.org/bot${TG_BOT_TOKEN}/editMessageText -d chat_id="$CHAT_ID" -d message_id="$MESSAGE_ID" -d text="$MESSAGE_TEXT" | jq .
 }
 
 LAST_COMMIT=$(git rev-parse --short HEAD)
 
-MESSAGE_ID=$(curl -s -X POST https://api.telegram.org/bot$TG_BOT_TOKEN/sendMessage -d chat_id="$TG_FLUID_MAINTAINERS_CHAT_ID" -d text="FluidCI started" | jq .result.message_id)
+CHAT_ID="-1001337426475"
+MESSAGE_TEXT="⚒ Fluid JSON checker
+
+A new commit has been pushed ($LAST_COMMIT)
+"
+
+MESSAGE="$(curl -s -X POST https://api.telegram.org/bot${TG_BOT_TOKEN}/sendMessage -d chat_id="$CHAT_ID" -d text="$MESSAGE_TEXT" | jq .)"
+echo "$MESSAGE"
+
+MESSAGE_ID="$(echo "$MESSAGE" | jq .result.message_id)"
+echo "Message ID: $MESSAGE_ID"
 
 edit_ci_message "First step: Checking changed JSONs"
 
 CHANGED_JSONS="$(git diff --name-only HEAD~1 . | grep .json)"
+
 if [ "$CHANGED_JSONS" = "" ]; then
 	edit_ci_message "No JSON has been added or changed!"
 	exit
@@ -36,7 +44,7 @@ else
 	if [ "$SYNTAX_CHECK_RESULT" != "failed" ]; then
 		edit_ci_message "All modified JSONs are correct!"
 	else
-		edit_ci_message "$TG_FLUID_MAINTAINERS_CHAT_ID" "There's a bad JSONs! @SebaUbuntu kill whoever did this commit and fix it"
+		edit_ci_message"There's a bad JSONs! @SebaUbuntu kill whoever did this commit and fix it"
 		exit
 	fi
 fi
